9df3717b1fc5ac0b39cd0c22a1fc7f09d25c1758
pranavvdesai <pranavdesai.psd@gmail.com>
2021-04-07 19:17:28 +0530

    modified

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,session
 import mysql.connector
-
+import os
 
 
 app= Flask(__name__)
-
+app.secret_key=os.urandom(24)
+ 
 conn=mysql.connector.connect(host="remotemysql.com",user="9YwiYaINDg",password="WB2u9rVHb5",database="9YwiYaINDg")
 cursor=conn.cursor()
 
@@ -16,12 +17,23 @@ def index():
 
 
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-
-
+@app.route('/home')
+def home():
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect('/')
+   
 
 @app.route("/loginvalidation", methods=['POST'])
 def loginval():
@@ -29,18 +41,35 @@ def loginval():
     password=request.form.get('password')
     cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE '{}' AND `password` LIKE '{}'""".format(email,password))
     users=cursor.fetchall()
-    print(users)
+    if len(users)>0:
+        session['user_id']=users[0][0]
+        return redirect('/home')
+    else:
+        return redirect('/login')
+
+
     # dic={}
     # dic[users[0][1]]=users[0][2]
     # return dic
-    return users
+    
 
 
+@app.route('/add_user',methods=['POST'])
+def add_user():
+    name=request.form.get('username')
+    email=request.form.get('useremail')
+    password=request.form.get('userpassword')
+    phone_number=request.form.get('userphone')
+
+    cursor.execute("""INSERT INTO `users` (`user_id`,`name`,`email`,`password`,`phone_number`) VALUES (NULL,'{}','{}','{}','{}')""".format(name,email,password,phone_number))
+    conn.commit()
+    return redirect('/login')
 
 
-
-
-
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
 
 
 
@@ -55,3 +84,4 @@ def loginval():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
